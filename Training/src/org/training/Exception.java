f57f0d13434b7d885a2fe293dbcf026559d063f5
akash35876 <ak35876@gmail.com>
2023-02-06 15:28:40 +0530

    New

---
+++ Training/src/org/training/Exception.java
@@ -0,0 +1,17 @@
+package org.training;
+
+public class Exception {
+	
+	private void ki() {
+		// TODO Auto-generated method stub
+System.out.println("Akash");
+	}
+	
+	public static void main(String[] args) {
+		
+		Exception a = new Exception();
+		
+		a.ki();
+	}
+
+}
